41a27a2eef1b2d67f5a0864c95593830922fef7d
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-06-01 18:19:26 +0600

    Create JavaScript Dynamic.js

---
+++ JavaScript Dynamic.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Dynamic Data Types</h2>
+<p>This means that the same variable can be used to hold different data types:</p>
+<p id="demo"></p>
+<script>
+let x;        
+x = 1252;        
+x = "Md Jakaria Nur";   
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+
