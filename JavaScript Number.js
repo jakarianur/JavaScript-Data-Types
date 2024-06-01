cd10cd93e0097e6000fd3ec43ef229b991bc964a
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-06-01 18:28:53 +0600

    Create JavaScript Number.js

---
+++ JavaScript Number.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Numbers</h2>
+<p>Numbers can be written with, or without decimals:</p>
+<p id="demo"></p>
+
+<script>
+let x1 = 98.00;
+let x2 = 100;
+let x3 = 52.14;
+document.getElementById("demo").innerHTML =
+x1 + "<br>" + x2 + "<br>" + x3;
+</script>
+
+</body>
+</html>
+
+
