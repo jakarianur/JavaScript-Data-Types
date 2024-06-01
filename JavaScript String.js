6595e4b9837965dcb5394eda93331c97ba78ae4e
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-06-01 18:24:43 +0600

    Create JavaScript String.js

---
+++ JavaScript String.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript String</h2>
+<p>Strings are written with quotes. You can use single or double quotes:</p>
+<h4 id="demo"></h4>
+<script>
+let Name1 = "Md Jakaria Nur";
+let Name2 = 'Md Jubayer Nur';
+document.getElementById("demo").innerHTML =
+Name1 + "<br>" + Name2; 
+</script>
+</body>
+</html>
